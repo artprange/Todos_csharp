52193a45691a6c4d428c9ef703521ef800e7a243
xprangex <arthur.prange@gmail.com>
2023-07-25 21:35:08 -0300

    primeiro commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.WebSockets;
+
+
+
+
+    Console.WriteLine("Hey let's calculate stuff!");
+
+
+var userChoice = "";
+while (userChoice != "E" || userChoice != "e")
+{
+
+    Console.WriteLine("First number:");
+string a = Console.ReadLine();
+int numberA = int.Parse(a);
+
+
+Console.WriteLine("Second number:");
+string b = Console.ReadLine();
+int numberB = int.Parse(b);
+
+Console.WriteLine("You choose the numbers: " + numberA + " and number: " + numberB);
+
+Console.WriteLine("Now, tell me. What you want to do with them?");
+
+
+Console.WriteLine("[M]ultiply");
+Console.WriteLine("[D]ivide");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[E]xit");
+
+
+    //função
+    void PrintFinalEquiation(
+    int numberA, int numberB, int result, string @operator)
+    {
+        Console.WriteLine("The result of " + numberA + " " + @operator + " " + numberB + " equals to: " + result);
+    }
+
+
+    //refatorando para aceitar maiusculo e minusculo
+    bool UpperOrLowerCase(string upper, string lower)
+    {
+        return upper.ToUpper() == lower.ToUpper();
+    }
+
+    userChoice = Console.ReadLine();
+if (UpperOrLowerCase(userChoice, "M"))
+    {
+        var multiplication = numberA * numberB;
+        PrintFinalEquiation(numberA, numberB, multiplication, " * ");
+   
+
+}
+
+else if (UpperOrLowerCase(userChoice, "D"))
+    {   var division = numberA / numberB;
+        PrintFinalEquiation(numberA, numberB, division, " / ");
+        
+
+
+    } else if (UpperOrLowerCase(userChoice, "A"))
+
+
+    {
+        var addition = numberA + numberB;
+        PrintFinalEquiation(numberA, numberB, addition, " + ");
+        
+
+
+    } else if (UpperOrLowerCase(userChoice, "S"))
+    {
+        var subtraction = numberA - numberB;
+        PrintFinalEquiation(numberA, numberB, subtraction, " - ");
+        
+    }
+ 
+}
+
+
+
+
+Console.ReadKey();
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Reflection;
+
+var todos = new List<string>();
+
+bool shallExit = false;
+while (!shallExit) {
+    Console.WriteLine();
+    Console.WriteLine("Hello");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a new TODO");
+Console.WriteLine("[R]emovee a TODOs");
+Console.WriteLine("[E]xit");
+
+
+
+//setando inputs
+
+
+//printando texto base
+void PrintSelecectedOption(string SelectedOption)
+{
+    Console.WriteLine("Selected option:" + SelectedOption);
+}
+
+var userChoice = Console.ReadLine();
+
+switch (userChoice)
+    {
+   case "E":
+   case "e":
+        shallExit=true;
+        break;
+        case "S":
+    case "s":
+        PrintSelecectedOption("See all TODOS");
+        break;
+    case "A":
+    case "a":
+            AddTodo();
+        break;
+    case "R":
+    case "r":
+            RemoveTodo();
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+    }
+}
+
+//adicionando uma tarefa
+void AddTodo()
+{
+    bool isValidDescription = false;
+    while(!isValidDescription) { 
+    Console.WriteLine("Enter a TODO description: ");
+    var description = Console.ReadLine();
+
+    if(description == "")
+    {
+        Console.WriteLine("The description cannot be empty");
+    } else if (todos.Contains(description))
+    {
+        Console.WriteLine("The TODO already exists!");
+    } else
+    {
+        isValidDescription = true;
+        todos.Add(description);
+    }
+    }
+}
+
+// mostrando todas as tarefas
+
+void SeeAllTodos()
+{
+    if (todos.Count == 0)
+    {
+        noTodosMessage();
+    }
+    else
+    {
+        for (int i = 0; i < todos.Count; i++)
+        {
+            string? todo = todos[i];
+            Console.WriteLine($"{i++}.{todos[i]}");
+        }
+    }
+}
+
+
+//deletando uma tarefa
+
+    void RemoveTodo()
+    {
+        if(todos.Count == 0)
+        {
+        noTodosMessage();
+            return;
+        }
+        bool isIndexValid = false;
+        while(!isIndexValid) { 
+        Console.WriteLine("Select the index of the TODO you want to purge");
+        SeeAllTodos();
+            var userInput = Console.ReadLine();
+            if(userInput == "")
+            {
+                Console.WriteLine("Selected index cannot be empty ");
+                continue;
+            }
+            if(int.TryParse(userInput, out int index)&&
+                index >=1 &&
+                index <= todos.Count)
+            {
+                var indexOfTodo = index - 1;
+                var todoToBeRemoved = todos[indexOfTodo];
+                todos.RemoveAt(indexOfTodo);
+                isIndexValid = true;
+                Console.WriteLine("TODO purged: " + todoToBeRemoved);
+            }
+            else { Console.WriteLine("the given index isn't valid.");
+            }
+        }
+    }
+
+Console.ReadKey();
+
+static void noTodosMessage()
+{
+    Console.WriteLine("No TODOS have been added yet");
+}
+
